ac9c74d6c7548ee3df740d17110bd1aecc3244fb
flyisland <flyisland@gmail.com>
2021-03-17 17:26:36 +0800

    fix: typo

--- src/main/java/semp/cfg/cli/BackupCommand.java
+++ src/main/java/semp/cfg/cli/BackupCommand.java
@@ -11,7 +11,7 @@ public class BackupCommand extends SubCommand {
     @CommandLine.Parameters(index = "0", completionCandidates = ResourceTypeCandidates.class,
             description = "Type of the exported object [${COMPLETION-CANDIDATES}]")
     private String resourceType;
-    @CommandLine.Parameters(index = "1..*", arity = "1..*", description = "One or more object names, , '*' means all")
+    @CommandLine.Parameters(index = "1..*", arity = "1..*", description = "One or more object names, , \"*\" means all")
     private String[] objectNames;
     @CommandLine.Option(names = {"-O", "---opaque-password"},
             description = "The opaquePassword for receiving and updating opaque properties like the password of Client Usernames")
