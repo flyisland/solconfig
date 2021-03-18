3b51fc43265198cccdcf428220523c5110f88203
flyisland <flyisland@gmail.com>
2021-03-18 16:29:16 +0800

    add Native Image Build Configuration files

---
+++ native-image.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+native-image --verbose --no-fallback --no-server \
+  --report-unsupported-elements-at-runtime \
+  --enable-http --enable-https \
+  -jar ./build/libs/sempcfg.jar
