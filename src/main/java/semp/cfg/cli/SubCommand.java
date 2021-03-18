11b3d65cae80476a2ff5f8242d77ed5a625397cc
flyisland <flyisland@gmail.com>
2021-03-18 08:50:17 +0800

    add: picocli annotationProcessor to support graalvm
    https://picocli.info/picocli-on-graalvm.html

--- src/main/java/semp/cfg/cli/SubCommand.java
+++ src/main/java/semp/cfg/cli/SubCommand.java
@@ -4,6 +4,7 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
+@CommandLine.Command(name = "subcommand", description = "abstract subcommand")
 public abstract class SubCommand implements Callable<Integer> {
     @CommandLine.ParentCommand
     protected SempCfgCommand parentCommand;
