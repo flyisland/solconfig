c0a85ee51aa9ab3089422bad09f86013756f2af9
flyisland <flyisland@gmail.com>
2021-04-06 20:08:07 +0800

    fix: add generated graalvm configuration files into the git repo again
    So that building Cross Platform Native Images later will be more easy, since the "test" subcommand in native-agent.sh needs to against a running PS+ broker.

--- native-agent.sh
+++ native-agent.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p ./src/main/resources/META-INF/native-image/solconfig
+./gradlew build
 java -agentlib:native-image-agent=config-output-dir=./src/main/resources/META-INF/native-image/solconfig \
-  -jar build/libs/solconfig.jar $@
+  -jar build/libs/solconfig.jar test
