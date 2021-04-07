34ef354113e0cc8009247a043bbf16192d8a967e
flyisland <flyisland@gmail.com>
2021-04-07 12:16:53 +0800

    refactor: turn native-agent.sh as a gradle task

--- native-agent.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ./src/main/resources/META-INF/native-image/solconfig
-./gradlew build
-java -agentlib:native-image-agent=config-output-dir=./src/main/resources/META-INF/native-image/solconfig \
-  -jar build/libs/solconfig.jar test
